db6ac79be7778661b8c543d3b1aa90800d69d5f4
Chaz <cmtlum73@gmail.com>
2016-01-21 09:43:34 -1000

    creates new table and starts to populate with existing data

--- normal_cars.sql
+++ normal_cars.sql
@@ -5,3 +5,18 @@ CREATE USER normal_user;
 DROP DATABASE IF EXISTS normal_cars;
 
 CREATE DATABASE normal_cars;
+
+\c normal_cars
+
+\i scripts/denormal_data.sql;
+
+CREATE TABLE IF NOT EXISTS car_makes
+(
+  "id" serial NOT NULL,
+  PRIMARY KEY( "id" ),
+  "brand" character varying(125) NOT NULL
+);
+
+INSERT INTO car_makes ( brand ) SELECT DISTINCT make_title FROM car_models ORDER BY make_title ASC;
+
+SELECT * FROM car_makes;
